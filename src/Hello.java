86c20bf37b78f95a5a00f384277f36368afb5ae2
Jayashree006 <jaishu006balaji@gmail.com>
2020-10-20 22:19:34 +0530

    Addition of two numbers

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,11 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		int num1=5;
+		int num2=10;
+		int sum = num1+num2;
+		System.out.println("sum of number:"+sum);
+		
 
 	}
 
